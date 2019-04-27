d556448da72aaafc5f7775c9fe186d2900466d40
saori <kamachaso.30@gmail.com>
2019-04-27 12:52:32 +0000

    add column

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,4 @@
 class Message < ApplicationRecord
     validates :content, presence: true, length: {maximum: 225}
+    validates :title, presence: true, length: {maximum: 225}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_19_082539) do
+ActiveRecord::Schema.define(version: 2019_04_27_022108) do
 
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
   end
 
 end
